87de8d6acfde6b9f79885ce2c549561d6f73f81d
alenwoo <719824@qq.com>
2018-11-23 01:01:45 +0800

    test

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,19 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Table1></Table1>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import Table1 from './components/Table1.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    HelloWorld,
+    Table1
   }
 }
 </script>
---
+++ src/components/Table1.vue
@@ -0,0 +1,17 @@
+<template>
+<div>
+  <el-radio disabled v-model="radio1" label="禁用">备选项</el-radio>
+  <el-radio disabled v-model="radio1" label="选中且禁用">备选项</el-radio>
+  </div>
+</template>
+
+<script>
+
+  export default {
+    data () {
+      return {
+        radio1: '选中且禁用'
+      };
+    }
+  }
+</script>
